f573d747697aa650088689bfe4eacff487149b70
Hung Nguyen <nthungdev@gmail.com>
2021-01-16 23:15:52 -0500

    Setup simble React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from 'components/navbar'
+import Home from 'pages/home'
+import Login from 'pages/login'
+import Register from 'pages/register'
 
 const App = () => {
   return (
-    <div className="bg-blue-600 h-screen text-white flex items-center">
-      <h1 className="text-center font-bold text-5xl w-full">Hello World!</h1>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
