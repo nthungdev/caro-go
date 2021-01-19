b9432363af995120df7b127bec69a80daba0acee
Hung Nguyen <nthungdev@gmail.com>
2021-01-19 01:02:12 -0500

    Refactor

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from 'components/navbar'
 import Home from 'pages/home'
 import Login from 'pages/login'
