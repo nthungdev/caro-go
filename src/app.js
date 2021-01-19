13c03534dba13c3113aad3132032b23108c1c7a1
Hung Nguyen <nthungdev@gmail.com>
2021-01-19 01:08:22 -0500

    Fix missing Redirect import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import Navbar from 'components/navbar'
 import Home from 'pages/home'
 import Login from 'pages/login'
